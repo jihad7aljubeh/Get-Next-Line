c20f091670cc8dcfb9cee57eb588536aa71e15dc
Jihad7aljubeh <jalju-be@student.42amman.com>
2025-12-10 21:08:38 +0300

    Get-Next-Line

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jalju-be <jalju-be@student.42amman.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/02 14:50:27 by jalju-be          #+#    #+#             */
+/*   Updated: 2025/10/19 15:48:39 by jalju-be         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+int		ft_strlen(const char *s);
+int		lentonew(const char *s);
+
+#endif
